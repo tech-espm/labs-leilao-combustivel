1b7acba77febb1ffe6194e1518956d897c81f887
LeonardoNunes_N <leonardonnegrao@outlook.com>
2020-11-21 18:19:40 -0300

    Criação model e API transportadora

---
+++ models/transportadora.ts
@@ -0,0 +1,85 @@
+import Sql = require("../infra/sql");
+
+export = class Transportadora {
+	public id_transp: number;
+	public nome_transp: string;
+	
+
+	private static validar(a: Transportadora): string {
+		a.nome_transp = (a.nome_transp || "").normalize().trim();
+		if (a.nome_transp.length < 3 || a.nome_transp.length > 100)
+			return "Nome inválido";
+
+		return null;
+	}
+
+	public static async listar(): Promise<Transportadora[]> {
+		let lista: Transportadora[] = null;
+
+		await Sql.conectar(async (sql: Sql) => {
+			lista = (await sql.query("select id_transp, nome_transp from transportadora order by nome_transp asc")) as Transportadora[];
+		});
+
+		return lista || [];
+	}
+
+	public static async obter(id_transp: number): Promise<Transportadora> {
+		let lista: Transportadora[] = null;
+
+		await Sql.conectar(async (sql: Sql) => {
+			lista = (await sql.query("select select id_transp, nome_transp from transportadora where id_transp = ?", [id_transp])) as Transportadora[];
+		});
+
+		return (lista && lista[0]) || null;
+	}
+
+	public static async criar(a: Transportadora): Promise<string> {
+		let res: string;
+		if ((res = Transportadora.validar(a)))
+			return res;
+
+		await Sql.conectar(async (sql: Sql) => {
+			try {
+				await sql.query("insert into transportadora (nome_transportadora) values (?)", [a.nome_transp]);
+			} catch (e) {
+			if (e.code && e.code === "ER_DUP_ENTRY")
+				res = `O assunto ${a.nome_transp} já existe`;
+			else
+				throw e;
+			}
+		});
+
+		return res;
+	}
+
+	public static async alterar(a: Transportadora): Promise<string> {
+		let res: string;
+		if ((res = Transportadora.validar(a)))
+			return res;
+
+		await Sql.conectar(async (sql: Sql) => {
+			try {
+				await sql.query("update transportadora set nome_transp = ? where id_transp = ?", [a.nome_transp, a.id_transp]);
+				res = sql.linhasAfetadas.toString();
+			} catch (e) {
+				if (e.code && e.code === "ER_DUP_ENTRY")
+					res = `A transportadora ${a.nome_transp} já existe`;
+				else
+					throw e;
+			}
+		});
+
+		return res;
+	}
+
+	public static async excluir(id_transp: number): Promise<string> {
+		let res: string = null;
+
+		await Sql.conectar(async (sql: Sql) => {
+			await sql.query("delete from transportadora where id_transp = ?", [id_transp]);
+			res = sql.linhasAfetadas.toString();
+		});
+
+		return res;
+	}
+};
---
+++ routes/api/transportadora.ts
@@ -0,0 +1,52 @@
+﻿import express = require("express");
+import wrap = require("express-async-error-wrapper");
+import jsonRes = require("../../utils/jsonRes");
+import Transportadora = require("../../models/perfil");
+import Usuario = require("../../models/usuario");
+
+const router = express.Router();
+
+// Se utilizar router.xxx() mas não utilizar o wrap(), as exceções ocorridas
+// dentro da função async não serão tratadas!!!
+router.get("/listar", wrap(async (req: express.Request, res: express.Response) => {
+	let u = await Usuario.cookie(req, res, true);
+	if (!u)
+		return;
+	res.json(await Transportadora.listar());
+}));
+
+router.get("/obter", wrap(async (req: express.Request, res: express.Response) => {
+	let u = await Usuario.cookie(req, res, true);
+	if (!u)
+		return;
+	let id = parseInt(req.query["id"] as string);
+	res.json(isNaN(id) ? null : await Transportadora.obter(id));
+}));
+
+router.post("/criar", wrap(async (req: express.Request, res: express.Response) => {
+	let u = await Usuario.cookie(req, res, true);
+	if (!u)
+		return;
+	let p = req.body as Transportadora;
+	jsonRes(res, 400, p ? await Transportadora.criar(p) : "Dados inválidos");
+}));
+
+router.post("/alterar", wrap(async (req: express.Request, res: express.Response) => {
+	let u = await Usuario.cookie(req, res, true);
+	if (!u)
+		return;
+	let p = req.body as Transportadora;
+	if (p)
+		p.id = parseInt(req.body.id);
+	jsonRes(res, 400, (p && !isNaN(p.id)) ? await Transportadora.alterar(p) : "Dados inválidos");
+}));
+
+router.get("/excluir", wrap(async (req: express.Request, res: express.Response) => {
+	let u = await Usuario.cookie(req, res, true);
+	if (!u)
+		return;
+	let id = parseInt(req.query["id"] as string);
+	jsonRes(res, 400, isNaN(id) ? "Dados inválidos" : await Transportadora.excluir(id));
+}));
+
+export = router;
