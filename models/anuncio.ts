1f1777e68f58ae85e8b2bb0b30a3f015d5b4f309
LeonardoNunes_N <leonardonnegrao@outlook.com>
2020-11-23 19:41:25 -0300

    Atualização model Anuncio

--- models/anuncio.ts
+++ models/anuncio.ts
@@ -7,7 +7,7 @@ export = class Anuncio {
 	public prazo_anu: string;
     public qtd_anu: number;
     public id_usuario: number; 
-	public data_anu: Date; 
+	public data_anu: string; 
     public valor_anu: number; 
     public id_transp: number; 
     public id_comb: number; 
@@ -19,7 +19,7 @@ export = class Anuncio {
 		let lista: Anuncio[] = null;
 
 		await Sql.conectar(async (sql: Sql) => {
-			lista = await sql.query("select u.id, u.login, u.nome, p.nome perfil, date_format(u.criacao, '%d/%m/%Y') criacao from usuario u inner join perfil p on p.id = u.idperfil order by u.login asc") as Anuncio[];
+			lista = (await sql.query("select id_anu, nome, date_format(criacao, '%d/%m/%Y') criacao from assunto order by nome asc")) as Anuncio[];
 		});
 
 		return (lista || []);
@@ -39,14 +39,14 @@ export = class Anuncio {
 	public static async criar(a: Anuncio): Promise<string> { 
 		let res: string;
 		await Sql.conectar(async (sql: Sql) => {
-			await sql.query("insert into anuncio (prazo_anu, qtd_anu, id_usuario, data_anu, valor_anu, id_transp, id_comb) values (?, ?, ?,?,?,?,?,?,?)", [a.prazo_anu, a.qtd_anu, a.id_usuario, a.data_anu, a.valor_anu, a.id_transp, a.id_comb]);
+			await sql.query("insert into anuncio (prazo_anu, qtd_anu, id_usuario, data_anu, valor_anu, id_transp, id_comb) values (?, ?, ?,now(),?,?,?,?,?)", [a.prazo_anu, a.qtd_anu, a.id_usuario, a.data_anu, a.valor_anu, a.id_transp, a.id_comb]);
 			res = sql.linhasAfetadas.toString();
 		});
 		
 		return res;
 	}
 
-	public static async editar(a: Anuncio): Promise<string> {
+	public static async alterar(a: Anuncio): Promise<string> {
 		let res: string;
 		await Sql.conectar(async (sql: Sql) => {
 			await sql.query("update anuncio set prazo_anu= ?, qtd_anu = ?, data_anu = ?, valor_anu = ?  where id_anu = ?", [a.prazo_anu, a.qtd_anu, a.data_anu, a.valor_anu]);
